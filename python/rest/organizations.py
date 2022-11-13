078d71737ce8702c3258d9fe93dd42e92010dfad
Han Nguyen <hannguyendev@gmail.com>
2022-11-12 20:01:35 -0800

    Update code skeleton for Organizations APIs

---
+++ python/rest/organizations.py
@@ -0,0 +1,28 @@
+import requests
+import json
+
+
+class Organizations:
+    def __init__(self):
+        pass
+    
+    def list_github_orgs(self):
+        pass
+    
+    def get_org_info(self):
+        pass
+    
+    def update_org_info(self):
+        pass
+    
+    def list_org_apps(self):
+        pass
+    
+    def enable_disable_org_security_product(self):
+        pass
+    
+    def list_auth_user_orgs(self):
+        pass
+    
+    def list_user_orgs(self):
+        pass
