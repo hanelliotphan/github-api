7d2f745043b6eeed016d6c5ab89949eb297be5f5
Han Nguyen <hannguyendev@gmail.com>
2022-11-18 21:32:21 -0800

    Update code skeleton (part 1)

--- python/rest/repositories.py
+++ python/rest/repositories.py
@@ -16,6 +16,8 @@ class Repositories:
             1. org (str): Name of the organization (from `https://github.com/<org>`)
         @return:
             response.json() (list) -- List of dictionaries, each of which represents information about a repository in the Github organization
+
+        Documentation: https://docs.github.com/en/rest/repos/repos#list-organization-repositories
         """
         try:
             response = requests.get(f"{self.git_endpoint}/orgs/{org}/repos", headers=self.headers)
@@ -25,3 +27,27 @@ class Repositories:
                 print(f"[ERROR] Could not get repositories from Github organization `{org}`\nStatus code: {response.status_code}\nReason: {response.reason}")
         except Exception as err:
             print(f"[ERROR] Could not get repositories from Github organization `{org}` with error: {err}")
+
+
+    def create_org_repo(self, org):
+        pass
+
+
+    def get_repo_info(self, owner, repo):
+        pass
+
+
+    def update_repo_info(self, owner, repo):
+        pass
+
+
+    def delete_repo(self, owner, repo):
+        pass
+
+
+    def enable_repo_automated_security_fixes(self, owner, repo):
+        pass
+
+    
+    def disable_repo_automated_security_fixes(self, owner, repo):
+        pass
