3c3be82bc28210f336bd0fafa362191c0ddcf8d5
Han Nguyen <hannguyendev@gmail.com>
2022-11-20 09:29:07 -0800

    Add function to create repository in Github organization

--- python/rest/repositories.py
+++ python/rest/repositories.py
@@ -1,5 +1,7 @@
-import requests
+import json
+import logging
 import os
+import requests
 
 
 class Repositories:
@@ -8,9 +10,9 @@ class Repositories:
         self.headers = {"Authorization": f"token {os.environ['GITHUB_PAT']}"}
 
 
-    def list_org_repos(self, org):
+    def list_repos_in_org(self, org):
         """
-        list_org_repos -- List all repositories of a Github organization
+        list_repos_in_org -- List all repositories of a Github organization
 
         @params:
             1. org (str): Name of the organization (from `https://github.com/<org>`)
@@ -29,8 +31,27 @@ class Repositories:
             print(f"[ERROR] Could not get repositories from Github organization `{org}` with error: {err}")
 
 
-    def create_org_repo(self, org):
-        pass
+    def create_repo_in_org(self, org, info):
+        """
+        create_repo_in_org -- Create a repository in a Github organization
+        Note: You must be an organization owner and your PAT must have `repo` enabled
+
+        @params:
+            1. org (str): Name of the organization (from `https://github.com/<org>`)
+            2. info (dict): Information about the repository to create (`name` in `info` is required)
+        @return:
+            None
+        
+        Documentation: https://docs.github.com/en/rest/repos/repos#create-an-organization-repository
+        """
+        try:
+            response = requests.post(f"{self.git_endpoint}/orgs/{org}/repos", data=json.dumps(info), headers=self.headers)
+            if 200 <= response.status_code < 300:
+                print(f"[INFO] Successfully created the repository `{info['name']}` in Github orgnanization `{org}`\nStatus code: {response.status_code}")
+            else:
+                print(f"[ERROR] Could not create a repository in Github organization `{org}`\nStatus code: {response.status_code}\nReason: {response.reason}")
+        except Exception as err:
+            print(f"[ERROR] Could not create a repository in Github organization `{org}` with error: {err}")
 
 
     def get_repo_info(self, owner, repo):
